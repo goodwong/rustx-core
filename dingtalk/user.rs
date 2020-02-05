bda8a70e562b0475d1ca58d1a9810236ed076e95
William Wong <goodwong@foxmail.com>
2020-02-05 15:59:44 +0000

    修改为可选字段

--- dingtalk/user.rs
+++ dingtalk/user.rs
@@ -2,36 +2,37 @@
 
 use super::dingtalk::Dingtalk;
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
+// use std::collections::HashMap;
 use std::error::Error;
 
 // 钉钉接口返回的类型定义
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct UserInfo {
-    union_id: String,                  // "PiiiPyQqBNBii0HnCJ3zljcuAiEiE"，不会改变
-    remark: String,                    // "remark",
-    user_id: String,                   // "zhangsan"，创建后不可修改
-    is_leader_in_depts: String,        // "{1:false}",
-    is_boss: bool,                     // false,
-    hired_date: u64,                   // 1520265600000,
-    is_senior: bool,                   // false,
-    tel: String,                       // "xxx-xxxxxxxx", 分机号（仅限企业内部开发调用）
-    department: Vec<i32>,              // [1,2],
-    work_place: String,                // "place",
-    email: String,                     // "test@xxx.com",
-    order_in_depts: String,            // "{1:71738366882504}",
-    mobile: String,                    // "1xxxxxxxxxx", 手机号码
-    active: bool,                      // false,
-    avatar: String,                    // "xxx",
-    is_admin: bool,                    // false, 是否为企业的管理员
-    is_hide: bool,                     // false,
-    job_number: String,                // "001",
-    name: String,                      // "张三",
-    ext_attr: HashMap<String, String>, // {}, 扩展属性，可以设置多种属性
-    state_code: String,                // "86",
-    position: String,                  // "manager",
-    roles: Vec<UserRole>,              // [{"id": 149507744, "name": "总监", "groupName": "职务"}]
+    userid: String,             // "zhangsan"，创建后不可修改
+    unionid: String,            // "PiiiPyQqBNBii0HnCJ3zljcuAiEiE"，不会改变
+    name: String,               // "张三",
+    tel: Option<String>,        // "xxx-xxxxxxxx", 分机号（仅限企业内部开发调用）
+    work_place: Option<String>, // "place",
+    remark: Option<String>,     // "remark",
+    mobile: String,             // "1xxxxxxxxxx", 手机号码
+    email: Option<String>,      // "test@xxx.com",
+    org_email: Option<String>,  // "test@xxx.com",
+    active: bool,               // false,
+    order_in_depts: String,     // "{1:71738366882504}",
+    is_admin: bool,             // false, 是否为企业的管理员
+    is_boss: bool,              // false,
+    is_leader_in_depts: String, // "{1:false}",
+    is_hide: bool,              // false,
+    department: Vec<i32>,       // [1,2],
+    position: String,           // "manager",
+    avatar: String,             // "xxx",
+    hired_date: u64,            // 1520265600000,
+    jobnumber: String,          // "001",
+    // extattr: HashMap<String, String>, // {}, 扩展属性，可以设置多种属性
+    is_senior: bool,      // false,
+    state_code: String,   // "86",
+    roles: Vec<UserRole>, // [{"id": 149507744, "name": "总监", "groupName": "职务"}]
 }
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
