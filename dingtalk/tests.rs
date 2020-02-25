17143974c83dd4fcc73728e071e3d658af850eb7
William Wong <goodwong@foxmail.com>
2020-02-25 09:19:26 +0000

    使用tokio::test简化测试代码

--- dingtalk/tests.rs
+++ dingtalk/tests.rs
@@ -8,38 +8,29 @@ fn read_config_from_env() {
     println!("cfg: {:#?}", cfg);
 }
 
-#[test]
-fn get_access_token() {
+#[tokio::test]
+async fn get_access_token() {
     let cfg = Config::from_env();
     let dd = Dingtalk::new(cfg);
 
-    let mut rt = Runtime::new().unwrap();
-    rt.block_on(async move {
-        println!("access_token: {}", dd.access_token().await.unwrap());
-    });
+    println!("access_token: {}", dd.access_token().await.unwrap());
 }
 
-#[test]
-fn get_user_info() {
+#[tokio::test]
+async fn get_user_info() {
     let dd = Dingtalk::new(Config::from_env());
 
-    let mut rt = Runtime::new().unwrap();
-    rt.block_on(async move {
-        let user_info = dd.user_info("manager7140".to_string()).await.unwrap();
-        println!("user_info: {:#?}", user_info);
-    });
+    let user_info = dd.user_info("manager7140".to_string()).await.unwrap();
+    println!("user_info: {:#?}", user_info);
 }
 
-#[test]
-fn auto_refresh_access_token() {
+#[tokio::test]
+async fn auto_refresh_access_token() {
     let dd = Dingtalk::new(Config::from_env());
     {
         let mut access_token = dd.access_token.lock().unwrap();
         *access_token = AccessToken::new("asdfasdfkajslkdfjals".to_string());
     }
 
-    let mut rt = Runtime::new().unwrap();
-    rt.block_on(async move {
-        let _ = dd.user_info("manager7140".to_string()).await.unwrap();
-    });
+    let _ = dd.user_info("manager7140".to_string()).await.unwrap();
 }
