ba1a65080c84c75eb0aad429977e68f936d484a2
William Wong <goodwong@foxmail.com>
2020-04-27 09:43:55 +0000

    将api里的client请求部分抽离出来

---
+++ api/client.rs
@@ -0,0 +1,228 @@
+use http::Method;
+use reqwest;
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use std::time::{Duration, Instant};
+use thiserror::Error as ThisError;
+use tokio::sync::RwLock;
+
+// 配置
+// 1. access_token 的response 2. varify错误类型
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct Config {
+    pub token_url: String,
+}
+impl Config {}
+
+// Client 结构
+pub struct Client {
+    cfg: Config,
+    token: AccessToken,
+}
+impl Client {
+    pub fn new(cfg: Config) -> Client {
+        Client {
+            cfg,
+            token: Default::default(),
+        }
+    }
+
+    pub async fn get<O>(&self, url: &str) -> ClientResult<O>
+    where
+        O: DeserializeOwned,
+    {
+        self.request(Method::GET, url, &()).await
+    }
+
+    pub async fn post<T, O>(&self, url: &str, payload: &T) -> ClientResult<O>
+    where
+        T: Serialize + ?Sized,
+        O: DeserializeOwned,
+    {
+        self.request(Method::POST, url, payload).await
+    }
+
+    pub async fn access_token(&self) -> ClientResult<String> {
+        let token = self.token.get_token().await;
+        if !token.valid() {
+            let new_token = self.fetch_access_token().await?;
+            let expires_in = Duration::from_secs(new_token.expires_in.unwrap_or(7200));
+            self.token
+                .set_token(AccessTokenEntiry::new(new_token.access_token, expires_in))
+                .await;
+        }
+        Ok(token.access_token)
+    }
+
+    pub async fn reset_access_token(&self, old_token: String) {
+        let mut token = self.token.0.write().await;
+        // 再次判断，避免排队reset
+        // 如token已经变化，
+        // 说明中间(因为锁，可能需要等待很久)有其他进程reset或fetch过这个token了，
+        // 则不再reset
+        if token.access_token == old_token {
+            *token = AccessTokenEntiry::default();
+        }
+    }
+
+    #[cfg(test)]
+    pub(crate) async fn set_invalid_access_token(&self) {
+        let mut token = self.token.0.write().await;
+        token.access_token = "invalid_access_token".to_owned();
+    }
+}
+
+impl Client {
+    async fn raw_request<T, O>(method: Method, url: &str, payload: &T) -> ClientResult<O>
+    where
+        T: Serialize + ?Sized,
+        O: DeserializeOwned,
+    {
+        // request...
+        let client = reqwest::Client::new();
+        let builder = match method {
+            Method::POST => client.post(url),
+            Method::GET => client.get(url),
+            _ => panic!("Invalid Mehtod: {}", method),
+        };
+        let response = builder.json(payload).send().await?.text().await?;
+        debug!("raw_request() response: {}", response);
+
+        // check error...
+        Self::check_error(&response)?;
+
+        // deserialize...
+        serde_json::from_str(&response).map_err(Into::into)
+    }
+
+    fn check_error(response: &str) -> ClientResult<()> {
+        let error: ApiErrorResponse = serde_json::from_str(response)?;
+        match error.errcode {
+            0 => Ok(()),
+            -1 => Err(ClientError::SystemBusy),
+            40001 | 40014 | 41001 => Err(ClientError::InvalidAccessToken),
+            _ => Err(ClientError::Other(error.errmsg)),
+        }?;
+        Ok(())
+    }
+
+    // 自动处理access_token
+    async fn request<T, O>(&self, method: Method, url: &str, payload: &T) -> ClientResult<O>
+    where
+        T: Serialize + ?Sized,
+        O: DeserializeOwned,
+    {
+        // auto retry...
+        let mut retry = 0;
+        loop {
+            // get access_token string
+            let token_str = if url.contains("ACCESS_TOKEN") {
+                self.access_token().await?
+            } else {
+                Default::default()
+            };
+            let url = url.clone().replace("ACCESS_TOKEN", &token_str);
+            let result = Self::raw_request(method.clone(), &url, payload).await;
+
+            retry += 1;
+            if retry > 2 {
+                break result;
+            }
+
+            // 除了无效token和系统繁忙需要进入重试，其它情况无论成功与否都退出循环
+            match &result {
+                Err(ClientError::InvalidAccessToken) => {
+                    self.reset_access_token(token_str).await;
+                }
+                Err(ClientError::SystemBusy) => {
+                    continue;
+                }
+                Ok(_) => break result,
+                Err(_) => break result,
+            }
+        }
+    }
+
+    async fn fetch_access_token(&self) -> ClientResult<ApiTokenResponse> {
+        let result: ApiTokenResponse =
+            Self::raw_request(Method::GET, &self.cfg.token_url, &()).await?;
+        Ok(result)
+    }
+}
+
+// api错误结构
+#[derive(Serialize, Deserialize, Debug)]
+struct ApiErrorResponse {
+    errcode: i32,
+    errmsg: String,
+}
+
+// 错误类型
+#[derive(ThisError, Debug)]
+pub enum ClientError {
+    #[error(transparent)]
+    Reqwest(#[from] reqwest::Error),
+    #[error(transparent)]
+    Serde(#[from] serde_json::error::Error),
+    #[error("Client Error InvalidAccessToken")]
+    InvalidAccessToken,
+    #[error("Client Error SystemBusy")]
+    SystemBusy,
+    #[error("Client Error:")]
+    Other(String),
+}
+pub type ClientResult<T> = Result<T, ClientError>;
+
+struct AccessToken(RwLock<AccessTokenEntiry>);
+impl Default for AccessToken {
+    fn default() -> Self {
+        AccessToken(RwLock::new(AccessTokenEntiry::default()))
+    }
+}
+impl AccessToken {
+    async fn get_token(&self) -> AccessTokenEntiry {
+        self.0.read().await.clone()
+    }
+    async fn set_token(&self, token: AccessTokenEntiry) {
+        *self.0.write().await = token
+    }
+}
+
+#[derive(Debug, Clone)]
+struct AccessTokenEntiry {
+    access_token: String,
+    expired_at: Instant,
+}
+impl Default for AccessTokenEntiry {
+    fn default() -> AccessTokenEntiry {
+        AccessTokenEntiry {
+            access_token: "".to_string(),
+            expired_at: Instant::now() - Duration::from_secs(10),
+        }
+    }
+}
+impl AccessTokenEntiry {
+    pub(crate) fn new(access_token: String, ttl: Duration) -> AccessTokenEntiry {
+        AccessTokenEntiry {
+            access_token,
+            expired_at: Instant::now() + ttl,
+        }
+    }
+    pub(crate) fn valid(&self) -> bool {
+        self.expired_at > Instant::now() + Duration::from_secs(10)
+    }
+}
+#[derive(Serialize, Deserialize, Debug)]
+struct ApiTokenResponse {
+    access_token: String,
+    expires_in: Option<u64>, // 钉钉没有这个字段
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn new_api_request() {
+        //let api_request = Client::new(Config{
+        //
+        //})
+    }
+}
---
+++ api/dingtalk.rs
@@ -0,0 +1,142 @@
+use super::client::{Client, ClientResult, Config as ClientConfig};
+use serde::{Deserialize, Serialize};
+
+// dingtalk 配置
+#[derive(Serialize, Deserialize, Debug, Default)]
+#[serde(rename_all = "camelCase")]
+pub struct Config {
+    pub corp_id: String,
+    pub agent_id: u64,
+    pub app_key: String,
+    pub app_secret: String,
+}
+impl Config {
+    pub fn from_env() -> Config {
+        use dotenv::dotenv;
+        use std::env;
+
+        dotenv().ok();
+        let corp_id = env::var("DINGTALK_CORP_ID")
+            .expect("value `DINGTALK_CORP_ID` not presented in .env file");
+        let agent_id = env::var("DINGTALK_AGENT_ID")
+            .expect("value `DINGTALK_AGENT_ID` not presented in .env file")
+            .parse()
+            .expect("value `DINGTALK_AGENT_ID` in .env file is not a valid integer");
+        let app_key = env::var("DINGTALK_APP_KEY")
+            .expect("value `DINGTALK_APP_KEY` not presented in .env file");
+        let app_secret = env::var("DINGTALK_APP_SECRET")
+            .expect("value `DINGTALK_APP_SECRET` not presented in .env file");
+
+        Config {
+            corp_id,
+            agent_id,
+            app_key,
+            app_secret,
+        }
+    }
+}
+
+// dingtalk 结构
+pub struct Dingtalk {
+    cfg: Config,
+    client: Client,
+}
+impl Dingtalk {
+    pub fn new(cfg: Config) -> Dingtalk {
+        let url = "https://oapi.dingtalk.com/gettoken?appkey=KEY&appsecret=SECRET";
+        let token_url = url
+            .replace("KEY", &cfg.app_key)
+            .replace("SECRET", &cfg.app_secret);
+        let client = Client::new(ClientConfig { token_url });
+
+        Dingtalk { cfg, client }
+    }
+
+    pub async fn access_token(&self) -> ClientResult<String> {
+        self.client.access_token().await
+    }
+
+    #[cfg(test)]
+    pub(crate) async fn set_invalid_access_token(&self) {
+        self.client.set_invalid_access_token().await
+    }
+}
+
+// 钉钉接口返回的类型定义
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct UserInfo {
+    userid: String,             // "zhangsan"，创建后不可修改
+    unionid: String,            // "PiiiPyQqBNBii0HnCJ3zljcuAiEiE"，不会改变
+    name: String,               // "张三",
+    tel: Option<String>,        // "xxx-xxxxxxxx", 分机号（仅限企业内部开发调用）
+    work_place: Option<String>, // "place",
+    remark: Option<String>,     // "remark",
+    mobile: String,             // "1xxxxxxxxxx", 手机号码
+    email: Option<String>,      // "test@xxx.com",
+    org_email: Option<String>,  // "test@xxx.com",
+    active: bool,               // false,
+    order_in_depts: String,     // "{1:71738366882504}",
+    is_admin: bool,             // false, 是否为企业的管理员
+    is_boss: bool,              // false,
+    is_leader_in_depts: String, // "{1:false}",
+    is_hide: bool,              // false,
+    department: Vec<i32>,       // [1,2],
+    position: String,           // "manager",
+    avatar: String,             // "xxx",
+    hired_date: u64,            // 1520265600000,
+    jobnumber: String,          // "001",
+    // extattr: HashMap<String, String>, // {}, 扩展属性，可以设置多种属性
+    is_senior: bool,      // false,
+    state_code: String,   // "86",
+    roles: Vec<UserRole>, // [{"id": 149507744, "name": "总监", "groupName": "职务"}]
+}
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct UserRole {
+    id: u32,            //
+    name: String,       // 角色名称
+    group_name: String, // 角色组名称
+}
+
+impl Dingtalk {
+    pub async fn user_info(&self, user_id: String) -> ClientResult<UserInfo> {
+        let url = "https://oapi.dingtalk.com/user/get?access_token=ACCESS_TOKEN&userid=USERID";
+        self.client.get(&url.replace("USERID", &user_id)).await
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Config, Dingtalk};
+
+    #[test]
+    fn read_config_from_env() {
+        let cfg = Config::from_env();
+        println!("cfg: {:#?}", cfg);
+    }
+
+    #[tokio::test]
+    async fn get_access_token() {
+        let cfg = Config::from_env();
+        let dd = Dingtalk::new(cfg);
+
+        println!("access_token: {}", dd.access_token().await.unwrap());
+    }
+
+    #[tokio::test]
+    async fn get_user_info() {
+        let dd = Dingtalk::new(Config::from_env());
+
+        let user_info = dd.user_info("manager7140".to_string()).await.unwrap();
+        println!("user_info: {:#?}", user_info);
+    }
+
+    #[tokio::test]
+    async fn auto_refresh_access_token() {
+        let dd = Dingtalk::new(Config::from_env());
+        dd.access_token().await.unwrap();
+        dd.set_invalid_access_token().await;
+        let _ = dd.user_info("manager7140".to_string()).await.unwrap();
+    }
+}
