172288cbff7ec5f6a49a6ad3c252c74ce1e72342
William Wong <goodwong@foxmail.com>
2020-05-03 04:39:37 +0000

    graphql context 增加miniprogram

--- auth/graphql/context.rs
+++ auth/graphql/context.rs
@@ -1,14 +1,21 @@
+use crate::api::wechat_miniprogram::Miniprogram;
 use crate::auth::service::Identity;
 use crate::db_connection::PgPool;
+use std::sync::Arc;
 
 pub struct Context {
     pub pool: PgPool,
     pub identity: Identity,
+    pub miniprogram: Arc<Miniprogram>,
     // todo 增加一个 session类，暂时包装 actix-session即可
 }
 impl juniper::Context for Context {}
 impl Context {
-    pub fn new(pool: PgPool, identity: Identity) -> Self {
-        Self { pool, identity }
+    pub fn new(pool: PgPool, identity: Identity, miniprogram: Arc<Miniprogram>) -> Self {
+        Self {
+            pool,
+            identity,
+            miniprogram,
+        }
     }
 }
